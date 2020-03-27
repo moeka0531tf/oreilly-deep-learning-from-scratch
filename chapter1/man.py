eec470e955449d52251c97555d55db19974abedb
中嶋萌花 <moeka@moeka-4.local>
2020-03-27 22:47:01 +0900

    add class sample

---
+++ chapter1/man.py
@@ -0,0 +1,14 @@
+class Man:
+    def __init__(self, name):
+        self.name = name
+        print('Intialized!')
+
+    def hello(self):
+        print("Hello " + self.name)
+
+    def goodbye(self):
+        print("goodbye" + self.name)
+
+m = Man("David")
+m.hello()
+m.goodbye()
